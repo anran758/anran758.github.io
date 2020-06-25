d98c6ca202c6e3fb03e03c283943d8583f920be3
anran758 <anran758@gmail.com>
2020-06-25 21:26:46 +0800

    test: 更新测试脚本

--- server.ts
+++ server.ts
@@ -1,20 +1,48 @@
+import fs from 'fs';
+import path from 'path';
 import express from 'express';
+import { green, red } from 'chalk';
 import config from './build/webpack/webpack.prod';
+import { exec } from 'child_process';
 
-const app = express();
 const PORT = 8001;
 
-// 将dist作为静态资源入口
-app.use(express.static(config?.output?.path || '8001'));
+const distPath = config?.output?.path || path.resolve(__dirname, 'dist');
 
-// 监听端口
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
+/**
+ * 启动测试服务器
+ */
+function startApp() {
+  const app = express();
 
-  console.log(
-    `Server is running at http://localhost:${PORT} . Press Ctrl+C to stop.\n`
-  );
-});
+  // 将dist作为静态资源入口
+  app.use(express.static(distPath));
+
+  // 监听端口
+  app.listen(PORT, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    console.log(
+      green(
+        `Project is running at http://localhost:${PORT}. Press Ctrl+C to stop.\n`
+      )
+    );
+  });
+}
+
+if (fs.existsSync(distPath)) {
+  startApp();
+} else {
+  const command = 'npm run build';
+  exec(command, (err) => {
+    if (err) {
+      console.log(red(`build error: \n\n${err}`));
+      return;
+    }
+
+    startApp();
+  });
+}
