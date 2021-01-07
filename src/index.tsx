f88dbe1bee05247fe7abf89df85f3a33e8c20ce2
anran758 <anran758@gmail.com>
2021-01-07 15:04:14 +0800

    refactor: 修改 component 引入路径，测试 Travis

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import './styles/global.less';
-import App from './pages/App';
+import App from './pages/App/index';
 
 ReactDOM.render(<App />, document.getElementById('root'));
